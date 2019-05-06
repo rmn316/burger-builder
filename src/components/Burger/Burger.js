260445160e1ed83ee96223117fc8a47a80f7e411
Robert Neal <rmn316@gmail.com>
2019-05-06 18:14:04 +0100

    Changed component css styles to *.module.css to make work with default webpack setup.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-import classes from './Burger.css';
+import classes from './Burger.module.css';
 
 const burger = () => {
     return (
