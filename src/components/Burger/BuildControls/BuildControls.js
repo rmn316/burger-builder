7a5d893d43444a89c978b719ab4715249c1f0400
Robert Neal <rmn316@gmail.com>
2019-05-07 16:57:45 +0100

    Added base build controls for user input.

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import classes from './BuildControls.module.css';
+
+const buildControls = (props) => (
+    <div>
+
+    </div>
+);
+
+export default buildControls;
+
--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -9,12 +9,17 @@ const burger = (props) => {
             return [...Array(props.ingredients[ingredientKey])].map((_, index) => {
                 return <BurgerIngredient key={ingredientKey+index} type={ingredientKey} />
             })
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+
+    console.log(transformedIngredients.length);
 
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            {transformedIngredients}
+            {transformedIngredients.length === 0 ? transformedIngredients : <p>Please start adding ingredients</p>}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
