aa693a2c90edaf0cbf1a344d408b1dc7cd41efee
Robert Neal <rmn316@gmail.com>
2019-05-07 20:47:49 +0100

    refactored Layout component into a hoc.

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Backdrop from '../Backdrop/Backdrop';
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 
 import classes from './Modal.module.css';
 
