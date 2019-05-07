f18f6bda269f09f4e09c8d424e9f2dc39c4d31a3
Robert Neal <rmn316@gmail.com>
2019-05-07 18:26:42 +0100

    Added buttons to order summary. including added custom button component.

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Aux from '../../../hoc/Aux';
+import Button from '../../UI/Button/Button';
 
 const orderSummary = (props) => {
 
@@ -8,7 +9,7 @@ const orderSummary = (props) => {
             return <li key={ingredientKey}>
                 <span style={{textTransform: 'capitalize'}}>{ingredientKey}</span>:{props.ingredients[ingredientKey]}
             </li>
-        })
+        });
 
     return (
         <Aux>
@@ -18,6 +19,8 @@ const orderSummary = (props) => {
                 {ingredientSummary}
             </ul>
             <p>Continue to checkout</p>
+            <Button buttonType="Danger" clicked={props.cancel}>CANCEL</Button>
+            <Button buttonType="Success" clicked={props.continue}>CONTINUE</Button>
         </Aux>
     )
 };
